c52f617d4e531e3acb94ea7212b8a35fb4656c1c
boven <boven.yan@gmail.com>
2015-12-21 01:27:45 -0500

    add cacheflow header

---
+++ CacheFlow.h
@@ -0,0 +1,16 @@
+#ifndef CACHE_FLOW_H
+#define CACHE_FLOW_H
+
+#include "stdafx.h"
+#include "Address.hpp"
+#include "Rule.hpp"
+#include "RuleList.h"
+
+class cache_unit{
+
+};
+
+class mixed_set{
+
+};
+#endif
