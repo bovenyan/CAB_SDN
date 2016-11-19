4fd3e57c47e468a5fe07d2aa2e3be36faae18572
Jiaren <jiaren@jiaren-Ubuntu-LabMachine.poly.edu>
2016-11-18 19:37:58 -0500

    formated the input of main() in TracePrepare.cpp and added its .ini

--- trace_gen/TracePrepare_blk.cpp
+++ trace_gen/TracePrepare_blk.cpp
@@ -31,31 +31,89 @@ void logging_init() {
     );*/
 }
 
-int main() {
+void print_usage() {
+    std::cerr << "Usage: TracePrepare_blk <TracePrepare_blk_config.ini> [rules_file] [tree_preallocate_data] [para_file_blk] [trace_root_dir]"
+              << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        print_usage();
+        return 1;
+    }
+
+    std::string rulesFileName;
+    std::string treePreallocateDataFileName;
+    std::string paraFileBlkFileName;
+    std::string traceRootDir;
+    int iThresholdHard;
+    bool bIsHotspot_prob_b_mutation;
+
+    std::ifstream iniFile(argv[1]);
+    std::string strLine;
+    std::vector<string> vector_parameters;
+
+    for (; std::getline(iniFile, strLine); ) {
+        vector_parameters = std::vector<string>();
+        boost::split(vector_parameters, strLine, boost::is_any_of(" \t"));
+        if ("rules_file" == vector_parameters[0]) {
+            rulesFileName = vector_parameters[1];
+        }
+        if ("tree_preallocate_data" == vector_parameters[0]) {
+            treePreallocateDataFileName = vector_parameters[1];
+        }
+        if ("para_file_blk" == vector_parameters[0]) {
+            paraFileBlkFileName = vector_parameters[1];
+        }
+        if ("trace_root_dir" == vector_parameters[0]) {
+            traceRootDir = vector_parameters[1];
+        }
+        if ("threshold_hard" == vector_parameters[0]) {
+            iThresholdHard = std::stoi(vector_parameters[1]);
+        }
+        if ("hotspot_prob_b_mutation" == vector_parameters[0]) {
+            if ("true" == vector_parameters[1]) bIsHotspot_prob_b_mutation = true;
+            else bIsHotspot_prob_b_mutation = false;
+        }
+    }
+
+    if (argc > 2) {
+        rulesFileName = std::string(argv[2]);
+    }
+    if (argc > 3) {
+        treePreallocateDataFileName = std::string(argv[3]);
+    }
+    if (argc > 4) {
+        paraFileBlkFileName = std::string(argv[4]);
+    }
+    if (argc > 5) {
+        traceRootDir = std::string(argv[5]);
+    }
+
+
     // init log, rule list, randomness
     srand (time(NULL));
     logging_init();
-    string rulefile = "../para_src/ruleset/rule8000-rgg";
-    rule_list rList(rulefile);
-    
+    rule_list rList(rulesFileName);
+
     // generate bucket tree
-    bucket_tree bTree(rList, 15);
+    bucket_tree bTree(rList, iThresholdHard);
     bTree.pre_alloc();
-    bTree.print_tree("../para_src/tree_pr.dat");
+    bTree.print_tree(treePreallocateDataFileName);
 
     // trace generation
     tracer tGen(&rList);
-    tGen.set_para("../para_src/para_file_blk.txt");
-    tGen.hotspot_prob_b(false);
+    tGen.set_para(paraFileBlkFileName);
+    tGen.hotspot_prob_b(bIsHotspot_prob_b_mutation);
 
-    tGen.trace_root_dir = "./Trace_Generate_blk/final/";
-    
-    for (int i = 1; i < 20; ++i){
-	tGen.flow_rate = 10*i;
-	tGen.hotspot_no = 50+2*i;
-	tGen.cold_prob = 0.001+0.0001*i;
-	tGen.hotvtime = 20;
-    	tGen.pFlow_pruning_gen(false);
+    tGen.trace_root_dir = traceRootDir;
+
+    for (int i = 1; i < 20; ++i) {
+        tGen.flow_rate = 10*i;
+        tGen.hotspot_no = 50+2*i;
+        tGen.cold_prob = 0.001+0.0001*i;
+        tGen.hotvtime = 20;
+        tGen.pFlow_pruning_gen(false);
     }
 
     //tGen.raw_snapshot("./Packet_File/sample-10-12", 10, 300);
