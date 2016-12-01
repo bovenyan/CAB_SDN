1ce914a0a228c74d2051ef420f3d0d21b5e2da3f
Bo Yan <boven.yan@gmail.com>
2016-12-01 10:47:33 -0500

    latency done

--- trace_gen/CalTrace.cpp
+++ trace_gen/CalTrace.cpp
@@ -14,7 +14,7 @@ using std::cout;
 using std::endl;
 using std::unordered_map;
 using std::vector;
-
+using std::ofstream;
 
 void print_help() {
     cerr << "Usage: FlowEcho {-s src_file -r dst_file}" << endl;
@@ -25,29 +25,65 @@ typedef struct header_addr {
     uint32_t dst_ip;
     uint16_t src_port;
     uint16_t dst_port;
-    uint16_t chksum;
+    uint16_t ip_id;
 
     bool operator==(const header_addr &other) const {
-        return (src_ip == other.src_ip && 
+        return (src_ip == other.src_ip &&
                 dst_ip == other.dst_ip &&
                 src_port == other.src_port &&
                 dst_port == other.dst_port &&
-                chksum == other.chksum);
+                ip_id == other.ip_id);
     }
 } header_addr;
 
-unordered_map<header_addr, vector<timeval> > record;
+struct header_hasher {
+    std::size_t operator()(const header_addr& hd) const {
+        using std::hash;
+
+        return (hash<uint32_t>()(hd.src_ip) ^
+                hash<uint32_t>()(hd.dst_ip) ^
+                hash<uint16_t>()(hd.src_port) ^
+                hash<uint16_t>()(hd.dst_port) ^
+                hash<uint16_t>()(hd.ip_id));
+    }
+};
+
+timeval tv_sub(const timeval & tv1, const timeval & tv2) {
+    // asume tv1 > tv2
+    int microseconds = (tv1.tv_sec - tv2.tv_sec) * 1000000 + ((int)tv1.tv_usec - (int)tv2.tv_usec);
+    timeval tv;
+    tv.tv_sec = microseconds/1000000;
+    tv.tv_usec = microseconds%1000000;
+
+    return tv;
+}
+
+int tv_cmp(const timeval & tv1, const timeval & tv2){
+    if (tv1.tv_sec < tv2.tv_sec)
+        return -1;
+    if (tv1.tv_sec > tv2.tv_sec)
+        return 1;
+
+    if (tv1.tv_usec < tv2.tv_usec)
+        return -1;
+    if (tv1.tv_usec > tv2.tv_usec)
+        return 1;
+    
+    return 0;
+}
+
+unordered_map<header_addr, vector<timeval>, header_hasher> record;
 
 void dump_TCP_packet(const unsigned char * packet, struct timeval ts,
                      unsigned int capture_len) {
-    if (capture_len < SIZE_ETHERNET){
+    if (capture_len < SIZE_ETHERNET) {
         return;
     }
 
     sniff_ethernet * eth = (sniff_ethernet *)packet;
 
     if (eth->ether_type != htons(ETHER_TYPE_IP))
-       return; 
+        return;
 
     sniff_ip * ip = (sniff_ip *)(packet + sizeof(sniff_ip));
 
@@ -56,13 +92,13 @@ void dump_TCP_packet(const unsigned char * packet, struct timeval ts,
     header_val.dst_ip = ip->ip_dst.s_addr;
     header_val.src_port = *(uint16_t*)(eth->ether_shost + 4);
     header_val.dst_port = *(uint16_t*)(eth->ether_dhost + 4);
+    header_val.ip_id = ip->ip_id;
 
-    if (record.find(header_val) != record.end()){
-        record[header_val].second.push_back(ts);
-    }    
-    else{
+    if (record.find(header_val) != record.end()) {
+        record[header_val].push_back(ts);
+    } else {
         vector<timeval> time_array(1, ts);
-        record[header_val].insert(std::make_pair(header_val, time_array));
+        record[header_val] = time_array;
     }
 }
 
@@ -109,11 +145,12 @@ int main(int argc, char * argv[]) {
     }
 
     if (!strcmp(src_file, "") || !strcmp(rcv_file, "")) {
+        cout <<"mising input"<<endl;
         print_help();
-        return 0;
+        return 1;
     }
 
-    // first file
+    // source file
     pcap_t * pd;
     char pebuf[PCAP_ERRBUF_SIZE];
     struct pcap_pkthdr header;
@@ -127,18 +164,57 @@ int main(int argc, char * argv[]) {
 
     pcap_close(pd);
 
-    // seoncd file 
-    /* pd = pcap_open_offline(rcv_file, pebuf);
-     * 
-     * while ((packet = pcap_next(pd, &header))) {
-     *     dump_TCP_packet(packet, header.ts, header.caplen);
-     * }
-     * pcap_close(pd); */
-
-    for (const timeval & rec : record) {
-        for (const timeval & ts : rec.pair){
-            cout<<ts.sec<<"."<<ts.usec<<"\t";
-        }
-        cout<<endl; 
+    // receive file
+    pd = pcap_open_offline(rcv_file, pebuf);
+    while ((packet = pcap_next(pd, &header))) {
+        dump_TCP_packet(packet, header.ts, header.caplen);
     }
+    pcap_close(pd);
+
+    ofstream output("latency.data");
+
+    int miss_pkt = 0;
+    int total_pkt = 0;
+
+    timeval rtt_min;
+    rtt_min.tv_sec = 1;
+    rtt_min.tv_usec = 0;
+    
+    timeval rtt_max;
+    rtt_max.tv_sec = 0;
+    rtt_max.tv_usec = 0;
+
+    for (const auto rec : record) {
+        ++total_pkt;
+
+        if (rec.second.size() != 4) {
+            ++miss_pkt;
+            continue;
+        }
+
+        timeval src_del = tv_sub(rec.second[1], rec.second[0]);
+        timeval dst_del = tv_sub(rec.second[3], rec.second[2]);
+        timeval rtt = tv_sub(src_del, dst_del);
+
+        output<<rtt.tv_sec<<"."<<rtt.tv_usec<<"\t";
+        output<<src_del.tv_sec<<"."<<src_del.tv_usec<<"\t";
+        output<<dst_del.tv_sec<<"."<<dst_del.tv_usec<<"\t";
+
+        if (tv_cmp(rtt, rtt_min) < 0)
+            rtt_min = rtt;
+
+        if (tv_cmp(rtt, rtt_max) > 0)
+            rtt_max = rtt;
+
+        for (const timeval & ts : rec.second) {
+            output<<ts.tv_sec<<"."<<ts.tv_usec<<"\t";
+        }
+        output<<endl;
+    }
+    output.close();
+
+    cout<<"max rtt: "<<rtt_max.tv_sec<<"."<<rtt_max.tv_usec<<endl;
+    cout<<"min rtt: "<<rtt_min.tv_sec<<"."<<rtt_min.tv_usec<<endl;
+
+    return 0;
 }
--- trace_gen/FlowGen.cpp
+++ trace_gen/FlowGen.cpp
@@ -38,8 +38,8 @@ namespace io = boost::iostreams;
 //     return ss.str();
 // }
 
-int make_pkt(const addr_5tup & header, uint8_t ** data, uint32_t * pkt_len) {
-
+int make_pkt(const addr_5tup & header, uint8_t ** data,
+             uint32_t * pkt_len, uint16_t idx = 0) {
     uint32_t payload_size = sizeof(timespec);
     uint32_t buffer_size = sizeof(sniff_ethernet) + sizeof(sniff_ip) +
                            sizeof(sniff_tcp) + payload_size;
@@ -68,6 +68,7 @@ int make_pkt(const addr_5tup & header, uint8_t ** data, uint32_t * pkt_len) {
     ip->ip_src.s_addr = htonl(header.addrs[0]);
     ip->ip_dst.s_addr = htonl(header.addrs[1]);
     ip->ip_len = htons(buffer_size - sizeof(sniff_ethernet));
+    ip->ip_id = htons(idx);
 
     /* make time stamp */
     timespec * timestamp = (timespec *)body;
@@ -93,7 +94,7 @@ int make_pkt_ipv6(const addr_5tup & header, uint8_t ** data, uint32_t * pkt_len)
     sniff_ipv6 * ip = (sniff_ipv6 *)(buffer+sizeof(sniff_ethernet));
     // sniff_tcp * tcp = (sniff_tcp *)(buffer + sizeof(sniff_ethernet) +
     sniff_icmp * icmp = (sniff_icmp *)(buffer + sizeof(sniff_ethernet) +
-                                      sizeof(sniff_ipv6));
+                                       sizeof(sniff_ipv6));
     // uint8_t * body = buffer + sizeof(sniff_ethernet) +
     //                 sizeof(sniff_ipv6) + sizeof(sniff_tcp);
 
@@ -137,23 +138,18 @@ int make_pkt_ipv6(const addr_5tup & header, uint8_t ** data, uint32_t * pkt_len)
     unsigned char gen_ip[16] = {0xfe, 0x80, 0x00, 0x00,
                                 0x00, 0x00, 0x00, 0x00,
                                 0xa2, 0x36, 0x9f, 0xff,
-                                0xfe, 0x71, 0x14, 0x04};
+                                0xfe, 0x71, 0x14, 0x04
+                               };
 
     memcpy(ip->ip_dst.s6_addr, dst_ip, 16);
     memcpy(ip->ip_src.s6_addr, gen_ip, 16);
 
     ip->ip_len = htons(buffer_size - sizeof(sniff_ethernet) - sizeof(sniff_ip));
     ip->ip_nxt = 58;
-    // TODO: calculate ICMP chksum 
+
     *icmp = sniff_icmp();
     memcpy(icmp->tar_ip.s6_addr, tar_ip, 16);
     memcpy(icmp->orig_mac, src_mac, 6);
-
-    //DEBUG
-    // std::cout<<"before making time stamp\n";
-    /* make time stamp */
-    // timespec * timestamp = (timespec *)body;
-    // clock_gettime(CLOCK_REALTIME, timestamp);
 
     *data = buffer;
     *pkt_len = buffer_size;
@@ -164,7 +160,7 @@ int make_pkt_ipv6(const addr_5tup & header, uint8_t ** data, uint32_t * pkt_len)
 }
 
 void print_help() {
-    cerr << "Usage: FlowGen {-s stats_file -i interface -f pcap_file -F factor -ipv6/ipv4}";
+    cerr << "Usage: FlowGen {-s stats_file -i interface -f pcap_file -F factor --ipv6/ipv4}";
     cerr << endl;
 }
 
@@ -174,7 +170,7 @@ int main(int argc, char * argv[]) {
     char if_name[10];
     char stat_file_str[100];
     int factor = 1;
-    int ipv6_flag = 1;
+    int ipv6_flag = 0;
 
     pcap_t * pd = nullptr;
     char pebuf[PCAP_ERRBUF_SIZE];
@@ -227,19 +223,15 @@ int main(int argc, char * argv[]) {
         }
     }
 
-    if (if_name == NULL || trace_file_str == NULL || stat_file_str == NULL) {
+    if (!strcmp(if_name, "") ||
+            !strcmp(trace_file_str, "") ||
+            !strcmp(stat_file_str, "")) {
         print_help();
         return 0;
     }
 
-    // printf("%s, %s, %s, %i, %i\n", trace_file_str, if_name, stat_file_str, ipv6_flag, factor);
-    // return 0;
-
     pd = pcap_open_live(if_name, MAX_ETHER_FRAME_LEN, 1,
                         READ_TIMEOUT, pebuf);
-
-    //DEBUG
-    std::cout<<"before sending"<<endl;
 
     /* start sending  */
     ifstream trace_file(trace_file_str);
@@ -249,10 +241,6 @@ int main(int argc, char * argv[]) {
         print_help();
         return 2;
     }
-
-
-    //DEBUG
-    std::cout<<"before try"<<endl;
 
     try {
         io::filtering_istream in;
@@ -264,52 +252,36 @@ int main(int argc, char * argv[]) {
         /* set birth time */
         clock_gettime(CLOCK_MONOTONIC, &zero.time_point_);
 
-        /* for recording packet sent */
-        // std::ofstream ofs;
-        // ofs.open("./flows", std::ofstream::out);
-        std::cout<<"before sending 2 flows"<<endl;
-        //for debugging use, send 20 flows.
-        int iTest2FLow = 20;
-        while(getline(in,line)&&iTest2FLow) {
-            --iTest2FLow;
+        uint16_t pkt_idx = 0;
+        int max_pkt_to_send = 20;
+
+        while(getline(in,line) && pkt_idx < max_pkt_to_send) {
+            ++pkt_idx;
+
             addr_5tup pkt_header(line);
 
             uint8_t * pkt = nullptr;
             uint32_t  pkt_len = 0;
-            //DEBUG
-            std::cout<<"before setting packet interval"<<endl;
+
             /* set packet interval */
             TimeSpec next_pkt_ts(pkt_header.timestamp * factor);
             clock_gettime(CLOCK_MONOTONIC, &now.time_point_);
-            //DEBUG
 
-            cout<<"before if expression"<<endl;
             if (now < zero + next_pkt_ts) {
                 TimeSpec to_sleep = next_pkt_ts + zero - now;
                 nanosleep(&to_sleep.time_point_, nullptr);
             }
 
-            // string ph_str (str_readable(pkt_header));
-            // if (flows.find(ph_str) == flows.end())
-            // {
-            //     flows.insert(ph_str);
-            //     ofs << ph_str << endl;
-            // }
-            //DEBUG
-            std::cout<<"before making ip packet\n";
             if (ipv6_flag) {
                 make_pkt_ipv6(pkt_header, &pkt, &pkt_len);
             } else {
-                make_pkt(pkt_header, &pkt, &pkt_len);
+                make_pkt(pkt_header, &pkt, &pkt_len, pkt_idx);
             }
-
-            cout<< "packet length == " << pkt_len << endl;
 
             int result = pcap_sendpacket(pd,pkt,pkt_len);
 
             delete [] pkt;
         }
-        // ofs.close();
     } catch(std::exception & e) {
         cerr << e.what() << endl;
     }
